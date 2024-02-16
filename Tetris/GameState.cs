8c3a4ebb7c8f690efc6688ba0c6a675a5b5f417f
Ilya Kunitsky <Ilyhhaa@gmail.com>
2024-02-16 13:57:12 +0300

    GameState: Queue, IsGameOver??, GameGrid

--- Tetris/GameState.cs
+++ Tetris/GameState.cs
@@ -8,6 +8,27 @@ namespace Tetris
 {
     public class GameState
     {
+        private Block currentBlock;
 
+        public Block CurrentBlock
+        {
+            get => currentBlock;
+            private set
+            {
+
+                currentBlock = value;
+                currentBlock.Reset();
+            }
+        }
+        public GameGrid GameGrid { get; }
+
+        public BlockQueue BlockQueue { get; }
+
+        public bool GameOver { get; private set; }
+
+        public GameState()
+        {
+                
+        }
     }
 }
